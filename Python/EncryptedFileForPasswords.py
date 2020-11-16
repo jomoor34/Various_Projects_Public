0c0afc2740417253d7d530efd99da17751b6c0d4
jomoor34 <jomoor34@gmail.com>
2020-11-16 14:49:26 -0700

    Added EncryptedFileForPasswords script

---
+++ Python/EncryptedFileForPasswords.py
@@ -0,0 +1,17 @@
+#This script creates encrypted file to store password or other keys using binary files.
+#Use this script in conjunction with the UpdateGoDaddyDNS script
+
+from cryptography.fernet import Fernet
+
+#Create key
+key = Fernet.generate_key()
+print("Encryption key: {0}".format(key))
+
+#Encrypt password
+cipher_suite = Fernet(key)
+ciphered_text = cipher_suite.encrypt(b"EnterPasswordOrAPIKeyHere")
+print("Ciphered text: {0}".format(ciphered_text)) #Must grab ciphered text from this step to use in the RetrieveEncryptedPassword program
+
+#Write password to file
+with open('/path/to/keyfolder/keyfile.bin', 'wb') as key_file:
+    key_file.write(ciphered_text)
